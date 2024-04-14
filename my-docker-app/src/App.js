4b2dc412c5ef3ed75e3e345d09f603b58fd768a0
ChandulaJ <chandugoods3000@gmail.com>
2024-04-14 18:30:59 +0530

    add react application

---
+++ my-docker-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My React App</h1>
+      <p>Learn Dockerizing with this awesome React app!</p>
+    </div>
+  );
+}
+
+export default App;
